7e962033ac2e09861cd714a05d955fb70a20244d
Saurav Pradhan <42857122+sauravpradhan1@users.noreply.github.com>
2017-05-14 20:17:19 +1000

    Preference Screen

---
+++ android/ServerMonitor/app/src/main/java/hem/server/monitor/GCMPreferences.java
@@ -0,0 +1,8 @@
+package hem.server.monitor;
+
+
+public class GCMPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
